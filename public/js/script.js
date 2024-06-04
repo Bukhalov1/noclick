9c6a363b00b9f75dc0c189a268219a6ed8f21797
bukhalov1 <buhalov.alexei@yandex.ru>
2024-06-05 01:09:47 +0300

    update

--- public/js/script.js
+++ public/js/script.js
@@ -17,7 +17,6 @@ function getUserMultiplier() {
 // Запуск обновления времени с интервалом в 1 секунду
 intervalId = setInterval(updateTimeSpent, 1000);
 
-
 function toggleMenu() {
     const bottomPanel = document.getElementById('bottomPanel');
     const toggleButton = document.getElementById('toggleButton');
@@ -25,8 +24,19 @@ function toggleMenu() {
     if (bottomPanel.classList.contains('show')) {
         bottomPanel.classList.remove('show');
         toggleButton.style.bottom = '20px';
+        toggleButton.textContent = "↑";
     } else {
         bottomPanel.classList.add('show');
-        toggleButton.style.bottom = '120px'; // Adjust based on the height of the bottom panel
+        toggleButton.style.bottom = '95px'; // Adjust based on the height of the bottom panel
+        toggleButton.textContent = "↓";
     }
+    toggleButton.classList.toggle('active');
 }
+
+// Resize event listener to handle dynamic resizing
+window.addEventListener('resize', () => {
+    document.querySelector('.container').style.height = `${window.innerHeight}px`;
+});
+
+// Set the initial height of the container
+document.querySelector('.container').style.height = `${window.innerHeight}px`;
